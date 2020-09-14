9afb1c70b0454f86f0bb7f33b7d5a9c002395591
Kasech Isaac <kasechisaac@Kasechs-MacBook-Air.local>
2020-09-13 19:00:43 -0700

    done

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,20 +1,38 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Component } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "./util/axiosWithAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
-const BubblePage = () => {
-  const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
+// fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
-
+class  BubblePage extends Component {
+  state = {
+    colorList:[]
+  };
+componentDidMount (){
+  this.clolerLists();
+}
+  
+  clolerLists =()=>{
+    axiosWithAuth().get(`/api/colors`)
+   .then(res=>{
+    this.setState({colorList:res.data})
+    console.log(res.data)
+})
+    .catch(err => console.log(err));
+  }
+  
+ 
+render(){
   return (
-    <>
-      <ColorList colors={colorList} updateColors={setColorList} />
-      <Bubbles colors={colorList} />
-    </>
+    <div>
+      <ColorList colors={this.colorList} updateColors={this.colorList} />
+      <Bubbles colors={this.colorList} />
+    </div>
   );
+}
 };
 
 export default BubblePage;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,70 @@
 import React from "react";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
+import { axiosWithAuth } from "./util/axiosWithAuth";
+
+// make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+ 
+class  Login extends React.Component{
+  state = {
+    credentials: {
+      username: "",
+      password: ""
+    }
+  };
+
+  
+ 
+  handleChange = (e) => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+    
+ 
+
+   theSubmit = e =>{
+    e.preventDefault();
+    axiosWithAuth()
+    .post("/api/login",this.state.credentials)
+
+    .then (res => {
+      window.localStorage.setItem('token', res.data.payload)
+      // this.props.history.push('/protected')
+  })
+  .catch (err => console.log(err))
+  }
+
+
+  render() {
   return (
-    <>
+    <div>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+
+      <form onSubmit={this.theSubmit}>
+          <input
+            type="text"
+            name="username"
+            placeholder="username"
+            value={this.state.credentials.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            value={this.state.credentials.password}
+            onChange={this.handleChange}
+          />
+          <button>Log in</button>
+        </form>
+    </div>
   );
+  }
 };
 
 export default Login;
